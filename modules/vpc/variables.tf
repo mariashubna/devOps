cdafafeb9d7ac1df952540baa6feb1f3dfa47d68
Mariia <mariashubna@gmail.com>
2025-11-03 16:21:33 +0100

    Add Terraform modules for S3, VPC, and ECR

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,5 @@
+variable "vpc_cidr_block" {}
+variable "public_subnets" { type = list(string) }
+variable "private_subnets" { type = list(string) }
+variable "availability_zones" { type = list(string) }
+variable "vpc_name" {}
